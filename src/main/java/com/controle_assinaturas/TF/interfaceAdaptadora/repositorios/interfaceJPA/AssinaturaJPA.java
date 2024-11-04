f80ed6415acfd0cbf8c45e3ee0a5782df9243ff0
Felipe <felipe.j03@edu.pucrs.br>
2024-11-04 14:49:46 -0300

    !!

--- src/main/java/com/controle_assinaturas/TF/interfaceAdaptadora/repositorios/interfaceJPA/AssinaturaJPA.java
+++ src/main/java/com/controle_assinaturas/TF/interfaceAdaptadora/repositorios/interfaceJPA/AssinaturaJPA.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.controle_assinaturas.TF.interfaceAdaptadora.repositorios.entidades.Assinatura;
 
 public interface AssinaturaJPA extends JpaRepository<Assinatura, Long> {
-    Optional<Assinatura> findById(Long codigo);
     Optional<Assinatura> findByClienteId(Long clienteId);
     Optional<Assinatura> findByAplicativoId(Long aplicativoId);
 }
