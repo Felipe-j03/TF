71707a5a945d6167f3741514ae9fe4d733000c70
Felipe <felipe.j03@edu.pucrs.br>
2024-11-06 13:33:29 -0300

    a

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -2,7 +2,7 @@ INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (1, 'StreamFlix', 40.0
 INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (2, 'FitLife', 15.0);
 INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (3, 'Easy Food', 10.0);
 INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (4, 'EducaVerso', 50.0);
-INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (5, 'Larissa', 20.0);
+INSERT INTO aplicativo (codigo, nome, custoMensal) VALUES (5, 'YouTube', 20.0);
 
 INSERT INTO cliente (codigo, nome, email) VALUES (10, 'Ricardo', 'ricardo@email.com');
 INSERT INTO cliente (codigo, nome, email) VALUES (20, 'Jonas', 'jonas@email.com');
@@ -21,7 +21,7 @@ INSERT INTO assinatura (codigo, aplicativo, cliente, inicioVigencia, fimVigencia
 INSERT INTO assinatura (codigo, aplicativo, cliente, inicioVigencia, fimVigencia) VALUES (4, 'EducaVerso', 'Larissa', '02-03-2024', '02-03-2025');
 INSERT INTO assinatura (codigo, aplicativo, cliente, inicioVigencia, fimVigencia) VALUES (5, 'Easy Food', 'Rafael', '15-10-2024', '15-01-2025');
 
-INSERT INTO pagamento (codigo, assinatura, valorPago, dataPagamento, promocao) VALUES (1, 1, 40.0, '10-05-2023', 'promo10');
+INSERT INTO pagamento (codigo, assinatura, valorPago, dataPagamento, promocao) VALUES (1, 1, 40.0, '10-05-2023', 'PAGUE_30_GANHE_45');
 INSERT INTO pagamento (codigo, assinatura, valorPago, dataPagamento, promocao) VALUES (2, 2, 15.0, '23-06-2024', NULL);
 INSERT INTO pagamento (codigo, assinatura, valorPago, dataPagamento, promocao) VALUES (3, 3, 40.0, '30-08-2024', NULL);
 INSERT INTO pagamento (codigo, assinatura, valorPago, dataPagamento, promocao) VALUES (4, 4, 50.0, '02-03-2024', NULL);
